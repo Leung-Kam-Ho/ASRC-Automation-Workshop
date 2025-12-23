9af3baca506bb2a964c55daa6b21bf58566d33e2
Kam Ho <71874669+Leung-Kam-Ho@users.noreply.github.com>
2025-12-23 12:21:16 +0800

    Refactor main function to improve readability and maintainability

--- main.py
+++ main.py
@@ -5,17 +5,24 @@ from robot_arm.position import Cartesian
 
 
 def main():
-    ra = RobotArm(host="192.168.1.64", base_offset=-90)
-    
+    # ra = RobotArm(host="192.168.1.64", base_offset=-90)
+    ra = RobotArm(host="192.168.1.48", base_offset=60)
+
+
+    # go to init pose
     ra.move_joint(ra.Init_pose)
-    
+
+    # wait until reach
     while ra.is_moving():
         pass
-    
+
+    # read current cartesian position
     ra.move_joint(jPos=ra.Init_pose)
-    
+
+    # wait until reach
     cartesian = ra.read_cartesian_pos()
-    
+
+    # move 100mm in x direction
     new_cartesian = Cartesian(
         x=cartesian.x + 100,
         y=cartesian.y,
@@ -24,8 +31,17 @@ def main():
         ry=cartesian.ry,
         rz=cartesian.rz
     )
-    
+
+    # move to new position
     ra.move_linear(cartesian=new_cartesian)
+
+
+    # wait until reach
+    while ra.is_moving():
+        pass
+
+    # go back to init pose
+    ra.move_joint(ra.Init_pose)
 
 if __name__ == "__main__":
     main()
